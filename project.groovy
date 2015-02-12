7b06735d6ab4ed1461fc72cfe0d25a28f16a0e25
devMauris <devMauris@users.noreply.github.com>
2015-02-12 14:36:43 +0300

    Run common job

--- project.groovy
+++ project.groovy
@@ -5,17 +5,20 @@ import java.util.concurrent.CancellationException
 
 // Retrieve parameters of the current build
 def jobname = build.buildVariableResolver.resolve("JOB")
-def gitlink = build.buildVariableResolver.resolve("GITLINK")
-String common_job_name = "common_" + jobname
-println "JOB=$jobname"
-println "GITLINK=$gitlink"
+def gituser = build.buildVariableResolver.resolve("GITUSER")
+def gitprj = build.buildVariableResolver.resolve("GITNAME")
+// Generate parameters for common build as well as common job name
+String githublink = "git@github.com:" + gituser + '/' + gitprj + '.git'
+String repolink = "https://github.com/" + gituser + '/' + gitprj + '.git'
+String common_jobname = "common_" + jobname.toLowerCase()
 
-def common_job = Hudson.instance.getJob(common_job_name.toLowerCase())
+def common_job = Hudson.instance.getJob(common_jobname)
 def new_build
 try {
     def params = [
       new StringParameterValue('JOB', jobname),
-      new StringParameterValue('GITLINK', gitlink),
+      new StringParameterValue('GITHUBLINK', githublink),
+      new StringParameterVakue('REPOLINK', repolink)
     ]
     def future = common_job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
     println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + common_job.url, common_job.fullDisplayName)
