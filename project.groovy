d857777d40b4e24cdfba744f510b4e5e42701a5f
devMauris <devMauris@users.noreply.github.com>
2015-02-12 13:25:33 +0300

    Basic job run

--- project.groovy
+++ project.groovy
@@ -1,3 +1,11 @@
+import hudson.model.*
+import hudson.AbortException
+import hudson.console.HyperlinkNote
+import java.util.concurrent.CancellationException
+
+// Retrieve parameters of the current build
+def jobname = build.buildVariableResolver.resolve("JOB")
+println "JOB=$jobname"
+
 //hey
 println "Heeey";
-
