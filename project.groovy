74f8ab99f01396ac0a97d656846e22e8afccfa0e
devMauris <devMauris@users.noreply.github.com>
2015-02-12 13:37:28 +0300

    Basic job run

--- project.groovy
+++ project.groovy
@@ -11,19 +11,19 @@ def common_job = Hudson.instance.getJob("common_test")
 def new_build
 try {
     def params = [
-      new StringParameterValue('FOO', foo),
+      new StringParameterValue("JOB", common_job),
     ]
-    def future = job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
-    println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + job.url, job.fullDisplayName)
-    anotherBuild = future.get()
+    def future = common_job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
+    println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + common_job.url, common_job.fullDisplayName)
+    new_build = future.get()
 } catch (CancellationException x) {
-    throw new AbortException("${job.fullDisplayName} aborted.")
+    throw new AbortException("${common_job.fullDisplayName} aborted.")
 }
-println HyperlinkNote.encodeTo('/' + anotherBuild.url, anotherBuild.fullDisplayName) + " completed. Result was " + anotherBuild.result
+println HyperlinkNote.encodeTo('/' + new_build.url, new_build.fullDisplayName) + " completed. Result was " + new_build.result
 
 // Check that it succeeded
-build.result = anotherBuild.result
-if (anotherBuild.result != Result.SUCCESS && anotherBuild.result != Result.UNSTABLE) {
+build.result = new_build.result
+if (new_build.result != Result.SUCCESS && new_build.result != Result.UNSTABLE) {
     // We abort this build right here and now.
     throw new AbortException("${anotherBuild.fullDisplayName} failed.")
 }
