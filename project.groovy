997f85b94cf45c3d58d55efe443063e1d4e26235
devMauris <devmauris@gmail.com>
2015-02-12 13:34:01 +0300

    Running test job

--- project.groovy
+++ project.groovy
@@ -7,5 +7,23 @@ import java.util.concurrent.CancellationException
 def jobname = build.buildVariableResolver.resolve("JOB")
 println "JOB=$jobname"
 
-//hey
-println "Heeey";
+def common_job = Hudson.instance.getJob("common_test")
+def new_build
+try {
+    def params = [
+      new StringParameterValue('FOO', foo),
+    ]
+    def future = job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
+    println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + job.url, job.fullDisplayName)
+    anotherBuild = future.get()
+} catch (CancellationException x) {
+    throw new AbortException("${job.fullDisplayName} aborted.")
+}
+println HyperlinkNote.encodeTo('/' + anotherBuild.url, anotherBuild.fullDisplayName) + " completed. Result was " + anotherBuild.result
+
+// Check that it succeeded
+build.result = anotherBuild.result
+if (anotherBuild.result != Result.SUCCESS && anotherBuild.result != Result.UNSTABLE) {
+    // We abort this build right here and now.
+    throw new AbortException("${anotherBuild.fullDisplayName} failed.")
+}
