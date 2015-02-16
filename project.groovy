c8deda0269fcf0dc66bb102c736b999c8a66b3ed
Michael Ponomarev <devMauris@users.noreply.github.com>
2015-02-16 16:41:38 +0300

    wut

--- project.groovy
+++ project.groovy
@@ -2,50 +2,37 @@ import hudson.model.*
 import hudson.AbortException
 import hudson.console.HyperlinkNote
 import java.util.concurrent.CancellationException
-
 // Retrieve parameters of the current build
 def jobname = build.buildVariableResolver.resolve("JOB")
 def gitlink = build.buildVariableResolver.resolve("GITLINK")
 def slavecustom = build.buildVariableResolver.resolve("SLAVECUSTOMNAME")
 def gitbranch = build.buildVariableResolver.resolve("GITBRANCH")
-
 // Generate parameters for common build as well as common job name
 //String repolink = "https://github.com/" + gituser + '/' + gitprj + '.git'
 String common_jobname = "common_" + jobname.toLowerCase()
 String slavename = slavecustom.toLowerCase() + "_slave"
-
 def common_job = Hudson.instance.getJob(common_jobname)
 def new_build
-
-//Specify parameters for downstream common build
-
 try {
    def params = [
       new StringParameterValue('GITLINK', gitlink),
       new StringParameterValue('SLAVENAME', slavename),
       new StringParameterValue('GITBRANCH', gitbranch),
-    ]
-   //Fix some current build parameters if needed
-   def current_params = [
-   new StringParameterValue('COMMONJOB', common_job),
-    ]
-   
-    //Thread.currentThread().executable.addAction( new ParametersAction(current_params))
-    def future = common_job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
-    println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + common_job.url, common_job.fullDisplayName)
-    new_build = future.get()
+   ]
+   def future = common_job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
+   println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + common_job.url, common_job.fullDisplayName)
+   new_build = future.get()
 } catch (CancellationException x) {
-    throw new AbortException("${common_job.fullDisplayName} aborted.")
+   throw new AbortException("${common_job.fullDisplayName} aborted.")
 }
 catch (NullPointerException x) {
-    println "No such common job as $jobname"
-    throw new AbortException("$jobname aborted.")
+   println "No such common job as $jobname"
+   throw new AbortException("$jobname aborted.")
 }
 println HyperlinkNote.encodeTo('/' + new_build.url, new_build.fullDisplayName) + " completed. Result was " + new_build.result
-
 // Check that it succeeded
 build.result = new_build.result
 if (new_build.result != Result.SUCCESS && new_build.result != Result.UNSTABLE) {
-    // We abort this build right here and now.
-    throw new AbortException("${new_build.fullDisplayName} failed.")
+// We abort this build right here and now.
+   throw new AbortException("${new_build.fullDisplayName} failed.")
 }
