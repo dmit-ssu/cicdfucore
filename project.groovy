92bd1da31995c5e6cd2bb1dbfbcb49def39d17eb
Michael Ponomarev <devMauris@users.noreply.github.com>
2015-02-16 16:52:19 +0300

    Artifacts

--- project.groovy
+++ project.groovy
@@ -19,11 +19,11 @@ try {
       new StringParameterValue('SLAVENAME', slavename),
       new StringParameterValue('GITBRANCH', gitbranch),
    ]
-   def current_params = new ParametersAction([
+   def current_params = [
       new StringParameterValue('COMMONJOB', common_jobname)
-   ])
+   ]
    
-   Thread.currentThread().executable.addAction(current_params)
+   Thread.currentThread().executable.addAction(new ParametersAction(current_params))
    def future = common_job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
    println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + common_job.url, common_job.fullDisplayName)
    new_build = future.get()
