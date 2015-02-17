aeeaf74716e49a8efa664401c236f93b35be2feb
Michael Ponomarev <devMauris@users.noreply.github.com>
2015-02-17 16:11:30 +0300

    Student builds

--- group.groovy
+++ group.groovy
@@ -13,38 +13,44 @@ def gitbranch = build.buildVariableResolver.resolve("GITBRANCH")
 // Generate parameters for common build as well as common job name
 String common_jobname = "common_" + jobname.toLowerCase()
 String slavename = slavecustom.toLowerCase() + "_slave"
+//Generating student map
 def students = [:]
-//get all links
-studentlist.splitEachLine(/\s(?=(http|ftp)\S*(.git))/, { students << [(it[0]) : (it[1])] })
+studentlist.splitEachLine(/\s(?=(http|ftp)\S*(.git))/, { if(it.size()==2) students << [(it[0]) : (it[1])] })
 println students.toMapString()
-def params = [
-      new StringParameterValue('GITLINK', gitlink),
-      new StringParameterValue('SLAVENAME', slavename),
-      new StringParameterValue('GITBRANCH', gitbranch),
-      new StringParameterValue('COMMONJOB', common_jobname),
-   ]
 
 def common_job = Hudson.instance.getJob(common_jobname)
 def new_build
-try {
-   //Sharing parameters between main build and common_job build
-   build.addAction(new ParametersAction(params))
-   def future = common_job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
-   println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + common_job.url, common_job.fullDisplayName)
-   new_build = future.get()
-} catch (CancellationException x) {
-   throw new AbortException("${common_job.fullDisplayName} aborted.")
-}
-catch (NullPointerException x) {
-   println "No such common job as $jobname"
-   throw new AbortException("$jobname aborted.")
-}
 
-println HyperlinkNote.encodeTo('/' + new_build.url, new_build.fullDisplayName) + " completed. Result was " + new_build.result
+def buildstudent = {
+      student ->
+      def params = [
+            new StringParameterValue('GITLINK', student.key),
+            new StringParameterValue('SLAVENAME', slavename),
+            new StringParameterValue('GITBRANCH', gitbranch),
+            new StringParameterValue('COMMONJOB', common_jobname),
+         ]
+      
+      try {
+         //Sharing parameters between main build and common_job build
+         build.addAction(new ParametersAction(params))
+         def future = common_job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params))
+         println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + common_job.url, common_job.fullDisplayName)
+         new_build = future.get()
+      } catch (CancellationException x) {
+         throw new AbortException("${common_job.fullDisplayName} aborted.")
+      }
+      catch (NullPointerException x) {
+         println "No such common job as $jobname"
+         throw new AbortException("$jobname aborted.")
+      }
+      
+      println HyperlinkNote.encodeTo('/' + new_build.url, new_build.fullDisplayName) + " completed. Result was " + new_build.result
+      
+      // Check that it succeeded
+      build.result = new_build.result
+      if (new_build.result != Result.SUCCESS && new_build.result != Result.UNSTABLE) {
+      // We abort this build right here and now.
+        // throw new AbortException("${new_build.fullDisplayName} failed.")
+      }
 
-// Check that it succeeded
-build.result = new_build.result
-if (new_build.result != Result.SUCCESS && new_build.result != Result.UNSTABLE) {
-// We abort this build right here and now.
-   throw new AbortException("${new_build.fullDisplayName} failed.")
 }
