3025ed357137fdcc585e16ea32c68287c0a6445b
Michael Ponomarev <devMauris@users.noreply.github.com>
2015-02-19 15:35:28 +0300

    Fixing project name

--- project.groovy
+++ project.groovy
@@ -18,7 +18,7 @@ def params = [
       new StringParameterValue('SLAVENAME', slavename),
       new StringParameterValue('GITBRANCH', gitbranch),
       new StringParameterValue('COMMONJOB', common_jobname),
-      new StringParameterValue('JOB', jobname.tr(" ", "_")),
+      new StringParameterValue('JOB', main_jobname.tr(" ", "_")),
    ]
 
 def common_job = Hudson.instance.getJob(common_jobname)
