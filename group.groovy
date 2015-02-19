476f8061123c5aabbae928492342c5361ccbaeb7
Michael Ponomarev <devMauris@users.noreply.github.com>
2015-02-19 14:59:42 +0300

    Normalizing names

--- group.groovy
+++ group.groovy
@@ -10,13 +10,16 @@ def gitlink = build.buildVariableResolver.resolve("GITLINK")
 def slavecustom = build.buildVariableResolver.resolve("SLAVECUSTOMNAME")
 def studentlist = build.buildVariableResolver.resolve("STUDENTLIST")
 def gitbranch = build.buildVariableResolver.resolve("GITBRANCH")
+
 // Generate parameters for common build as well as common job name
 String common_jobname = "common_" + jobname.toLowerCase()
 String slavename = slavecustom.toLowerCase() + "_slave"
+
 //Generating student map
 def students = [:]
 studentlist.splitEachLine(/\s(?=(http|ftp)\S*(.git))/, {
       if(it.size()==2){
+            //removing spaces from names
             def splits = it[0].split()
             it[0] = ""
             splits.each({str -> it[0] += str})
@@ -24,11 +27,13 @@ studentlist.splitEachLine(/\s(?=(http|ftp)\S*(.git))/, {
       }
       
 })
+//#DEBUG#
 println students.toMapString()
 
 def common_job = Hudson.instance.getJob(common_jobname)
 def new_build
 def futures = []
+//Schedule student builds
 students.each({
       student ->
       def params = [
@@ -36,15 +41,15 @@ students.each({
             new StringParameterValue('SLAVENAME', slavename),
             new StringParameterValue('GITBRANCH', gitbranch),
             new StringParameterValue('COMMONJOB', common_jobname),
-            new StringParameterValue('JOB', jobname), ///student.key
+            new StringParameterValue('JOB', student.key),
          ]
       
       try {
          //Sharing parameters between main build and common_job build
+         //main build parameters have to be updated to collect artifacts correctly
          build.addAction(new ParametersAction(params))
          futures.add(common_job.scheduleBuild2(0, new Cause.UpstreamCause(build), new ParametersAction(params)))
-         println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + common_job.url, common_job.fullDisplayName)
-         //new_build = future.get()
+         println "Waiting for the completion of " + HyperlinkNote.encodeTo('/' + common_job.url, common_job.fullDisplayName) + "for " + student.key
       } catch (CancellationException x) {
          throw new AbortException("${common_job.fullDisplayName} aborted.")
       }
@@ -52,21 +57,12 @@ students.each({
          println "No such common job as $jobname"
          throw new AbortException("$jobname aborted.")
       }
-      
-      //println HyperlinkNote.encodeTo('/' + new_build.url, new_build.fullDisplayName) + " completed. Result was " + new_build.result
-      
-      // Check that it succeeded
-      //build.result = new_build.result
-      //throw new AbortException("${new_build.fullDisplayName} failed.")
-      //if (new_build.result != Result.SUCCESS && new_build.result != Result.UNSTABLE) {
-      // We abort this build right here and now.
-        // throw new AbortException("${new_build.fullDisplayName} failed.")
-      ///}
-
 })
 
-futures.each(
-      {
-            future -> new_build = future.get() 
-            println HyperlinkNote.encodeTo('/' + new_build.url, new_build.fullDisplayName) + " completed. Result was " + new_build.result
-      })
+//Get results for those builds
+futures.each({
+      future -> new_build = future.get() 
+      println HyperlinkNote.encodeTo('/' + new_build.url, new_build.fullDisplayName) + " completed. Result was " + new_build.result
+})
+
+build.result
